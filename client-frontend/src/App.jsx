5d0d6d57f9d661628c3e78b27a1db3229be38198
kwoeser <karmawoeser1@gmail.com>
2024-09-14 15:17:52 -0700

    dakpa plumbing website started

---
+++ client-frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Navbar from './Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar></Navbar>
+
+      {/* Main content of your app */}
+      <div className="container mx-auto p-4 text-3xl font-semi-bold">
+        <h2>Call Now!</h2>
+        <h2>Any Job, We will fix it.</h2>
+        <h2>1-800-000-000</h2>
+      </div>
+  
+    </>
+  )
+}
+
+
+export default App
---
+++ client-frontend/src/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Navbar() {
+return (
+
+    <nav className="bg-[#E97910] p-4">
+        <div className="container mx-auto flex justify-between items-center">
+            {/* Brand */}
+            <div className="text-[#64AB71] justify-between text-2xl font-bold">Bheopo Plumbing</div>
+
+            {/* Links (hidden on mobile NEEDS TO BE UPDATED FOR MOBILE LATER) */}
+            <ul className="hidden md:flex space-x-6 ml-8 text-white ">
+                <li><a href="#" className="hover:text-[#64AB71] hover:scale-105 transition-transform">Home</a></li>
+                <li><a href="#" className="hover:text-[#64AB71]">About</a></li>
+                <li><a href="#" className="hover:text-[#64AB71]">Services</a></li>
+                <li><a href="#" className="hover:text-[#64AB71]">Contact Us</a></li>
+            </ul>
+                
+            {/* Phone Number */}
+            <div className="text-white text-lg ml-auto md:ml-0">1-800-000-000</div>
+
+            {/* <ul className="hidden md:flex space-x-6 text-white">
+                <li><a href="#" className="hover:text-[#64AB71] hover:scale-105 transition-transform">Home</a></li>
+                <li><a href="#" className="hover:text-[#64AB71]">About</a></li>
+                <li><a href="#" className="hover:text-[#64AB71]">Services</a></li>
+                <li><a href="#" className="hover:text-[#64AB71]">Contact Us</a></li>
+            </ul> */}
+        </div>
+       
+    </nav>
+)
+}
