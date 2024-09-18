fc66af2aabba9025bf5d0d2be7f6086550cdb5b5
kwoeser <karmawoeser1@gmail.com>
2024-09-17 20:07:38 -0700

    redricintg index.html file added to root(dakpa) dir

--- client-frontend/src/App.jsx
+++ client-frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Navbar></Navbar>
 
       {/* Main content of your app */}
-      <div className="container mx-auto p-4 text-3xl font-semi-bold">
+      <div className="container mx-auto p-4 text-3xl font-semi-bold bg-white">
         <h2>Call Now!</h2>
         <h2>Any Job, We will fix it.</h2>
         <h2>1-800-000-000</h2>
